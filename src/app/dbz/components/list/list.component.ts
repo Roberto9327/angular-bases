95159eaa1337548a562f09f67b64a07dde7082a4
Roberto Rivero <roberivero@nuevatel.net>
2023-07-11 16:25:28 -0400

    clase 75

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -16,9 +16,14 @@ export class ListComponent {
   }]
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
-  onDeleteCharacter(index:number):void{
-    this.onDelete.emit( index );
+
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter(id?:string):void{
+    if ( !id ) return;
+
+    this.onDelete.emit( id );
      }
 
 }
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core'
+import { v4 as uuid } from 'uuid'
 import { Character } from '../interfaces/form.interfaces';
 
 @Injectable({
@@ -8,23 +9,29 @@ import { Character } from '../interfaces/form.interfaces';
 export class DbzService {
 
   public characters: Character[]= [{
+    id:uuid(),
     name: 'krillin',
     power: 1000
    },{
+    id:uuid(),
     name: 'Goku',
     power: 9500
    },{
+    id:uuid(),
     name: 'Vegeta',
     power: 7500
    }];
 
-   onNewCharacter( form:Character ):void{
-   this.characters.push(form);
+   addNewCharacter( form:Character ):void{
+    const newForm: Character = { id:uuid(), ...form };
+
+
+    this.characters.push(newForm);
 
    }
-
-   onDeleteCharacter(index:number): void{
-  this.characters.splice(index,1);
+   //onDeleteCharacter(index:number): void{
+   deleteCharacter(id:string){
+  this.characters = this.characters.filter(character => character.id !== id);
    }
 
   constructor(){
